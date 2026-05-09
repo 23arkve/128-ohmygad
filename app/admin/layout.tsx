2c536d94dd2973dbfaacd7f75ed585203a6cf5a8
JB <99584741+ultsjbn@users.noreply.github.com>
2026-05-09 20:20:08 +0800

    add: export const dynamic
    render these routes at request time

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
 import { redirect } from "next/navigation";
 import { getCurrentUserWithRole } from "@/lib/auth/get-current-user";
