f49c51d912fad94fbe40701bac55a198cfa2f1ec
JB <99584741+ultsjbn@users.noreply.github.com>
2026-05-11 23:37:20 +0800

    feat: increase event title character limit from 300 to 500

--- components/admin/event-form.tsx
+++ components/admin/event-form.tsx
@@ -256,7 +256,7 @@ export default function EventForm({ initialData, mode, onSuccess, onCancel }: Ev
                 prefixIcon={<Type size={15} />}
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
-                maxLength={300}
+                maxLength={500}
               />
 
               <div className="input-wrap">
