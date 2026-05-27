108c5e047e15a437c2e7e9c2c901a8ca5fa4af11
JB <99584741+ultsjbn@users.noreply.github.com>
2026-05-27 13:01:15 +0800

    fix: change divider color

--- components/shared/guidelines-page.tsx
+++ components/shared/guidelines-page.tsx
@@ -289,7 +289,7 @@ export default function GuidelinesPage() {
 			>
 				{detailGuideline && (
 					<div className="flex flex-col gap-4 pb-8 md:pb-1">
-						<div className="divider mt-0 mb-2 border-t border-black" />
+						<div className="divider mt-0 mb-2" />
 
 						<div className="space-y-2">
 							<p className="font-semibold text-sm uppercase tracking-wider text-gray-500">
